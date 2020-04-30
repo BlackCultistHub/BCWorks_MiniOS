9ce27e29e263ff946a53d81084082cc819c18856
BlackCultist <tema-vil@bk.ru>
2020-04-30 07:57:47 +0300

    startup fix

--- main.lua
+++ main.lua
@@ -1,7 +1,5 @@
 --os.pullEvent = os.pullEventRaw
 
-function main()
-    while true do
-        print("Test message from main module")
-    end
+while true do
+    print("Test message from main module")
 end
